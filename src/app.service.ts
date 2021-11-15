9e81c86e3ff05016812c723df9970011bf576493
YeomJaeSeon <a89541457@gmail.com>
2021-11-15 23:37:27 +0900

    Setting: 초기 세팅 - sqlite3 설치 - typeorm 설정 - subject entity 생성 - .env 설정파일 생성

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,7 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
-  getTest(): string{
+  getTest(): string {
     return 'hello test';
   }
 }
---
+++ src/subject/subject.entity.ts
@@ -0,0 +1,46 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Subject {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @CreateDateColumn({ name: 'created_at', nullable: false })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at', nullable: false })
+  updatedAt: Date;
+
+  @Column({ name: 'name', nullable: false })
+  name: string;
+
+  @Column({ name: 'number', unique: true, nullable: false })
+  number: string;
+
+  @Column({ name: 'period', nullable: true })
+  period: string;
+
+  @Column({ name: 'range', nullable: true })
+  range: string;
+
+  @Column({ name: 'type', nullable: true })
+  type: string;
+
+  @Column({ name: 'agency', nullable: true })
+  agency: string;
+
+  @Column({ name: 'step', nullable: true })
+  step: string;
+
+  @Column({ name: 'target_count', nullable: true })
+  targetCount: number;
+
+  @Column({ name: 'department', nullable: true })
+  department: string;
+}
