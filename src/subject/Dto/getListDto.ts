5f3b505c0b8da7700af3d00762c7ef581043fe4b
ji-park <qkrskdjf@naver.com>
2021-11-16 01:01:42 +0900

    Feat : 임상리스트 기능 추가

---
+++ src/subject/Dto/getListDto.ts
@@ -0,0 +1,9 @@
+import { IsNotEmpty } from "class-validator";
+
+export class GetListDto {
+    @IsNotEmpty()
+    limit: number;
+
+    @IsNotEmpty()
+    offset: number;
+}
--- src/subject/subject.service.ts
+++ src/subject/subject.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Connection, Repository } from 'typeorm';
+import { Subject } from './subject.entity';
+import { GetListDto } from './Dto/getListDto'
 @Injectable()
-export class SubjectService {}
+export class SubjectService {
+
+    constructor(
+        @InjectRepository(Subject) private subjectRepository: Repository<Subject>,
+        private connection: Connection
+    ) {
+
+    }
+
+    async getPostList(pageInfo: GetListDto) {
+        const subjectList = await
+            this.subjectRepository
+                .createQueryBuilder('subject')
+                .orderBy('subject.id', 'DESC')
+                .limit(pageInfo.limit)
+                .offset(pageInfo.offset)
+                .disableEscaping()
+                .getMany()
+        const subjectCount = await
+            this.subjectRepository
+                .count();
+        return { subjectList, subjectCount };
+    }
+
+    async getPostListByUpdate() {
+
+    }
+}
