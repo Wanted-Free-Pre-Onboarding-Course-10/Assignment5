5397b3c35bbdbbab0f8f68b6bf8a17abdce6f843
YeomJaeSeon <a89541457@gmail.com>
2021-11-17 02:24:47 +0900

    Fix: graphql 응답 생성자 변경

--- src/subject/dto/subject.graphql.res.ts
+++ src/subject/dto/subject.graphql.res.ts
@@ -16,65 +16,65 @@ export class SubjectGraphqlResDto {
     createdDate: string,
     updatedDate: string,
   ) {
-    this.id = id;
-    this.name = name;
-    this.number = number;
-    this.range = range;
-    this.period = period;
-    this.type = type;
-    this.agency = agency;
-    this.step = step;
-    this.targetCount = targetCount;
-    this.department = department;
-    this.createdDate = createdDate;
-    this.updatedDate = updatedDate;
+    if (id) this.id = id;
+    if (name) this.name = name;
+    if (number) this.number = number;
+    if (range) this.range = range;
+    if (period) this.period = period;
+    if (type) this.type = type;
+    if (agency) this.agency = agency;
+    if (step) this.step = step;
+    if (targetCount) this.targetCount = targetCount;
+    if (department) this.department = department;
+    if (createdDate) this.createdDate = createdDate;
+    if (updatedDate) this.updatedDate = updatedDate;
   }
 
   // pk
   @Field({ nullable: true })
-  id!: number;
+  id?: number;
 
   // 과제 명
   @Field({ nullable: true })
-  name!: string;
+  name?: string;
 
   // 과제 번호 (unique)
   @Field({ nullable: true })
-  number!: string;
+  number?: string;
 
   //연구 기간
   @Field({ nullable: true })
-  period!: string;
+  period?: string;
 
   //연구 범위
   @Field({ nullable: true })
-  range!: string;
+  range?: string;
 
   // 연구 종류
   @Field({ nullable: true })
-  type!: string;
+  type?: string;
 
   // 연구 책임 기관
   @Field({ nullable: true })
-  agency!: string;
+  agency?: string;
 
   // 임상 시험 단계
   @Field({ nullable: true })
-  step!: string;
+  step?: string;
 
   // 피연구자 수
   @Field({ nullable: true })
-  targetCount!: number;
+  targetCount?: number;
 
   // 진료 과
   @Field({ nullable: true })
-  department!: string;
+  department?: string;
 
   // 생성 날짜
   @Field({ nullable: true })
-  createdDate!: string;
+  createdDate?: string;
 
   // 수정 날짜
   @Field({ nullable: true })
-  updatedDate!: string;
+  updatedDate?: string;
 }
